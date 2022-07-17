f2ab6920404b26986a308c51f57ed20600b94eb5
Kevin Dunn <kgdunn@gmail.com>
2022-07-17 21:24:16 +0000

    Add tests to the app.

--- server/catalog/tests/test_views.py
+++ server/catalog/tests/test_views.py
@@ -3,27 +3,35 @@
 from rest_framework.test import APIClient, APITestCase
 
 from catalog.models import Wine
+from catalog.serializers import WineSerializer
 
 
 class ViewTests(APITestCase):
+    fixtures = ["test_wines.json"]
 
+    def setUp(self):
+        self.client = APIClient()
+
+    # changed
     def test_empty_query_returns_everything(self):
-        wine = Wine.objects.create(
-            country='US',
-            description='A delicious bottle of wine.',
-            points=90,
-            price=100.00,
-            variety='Cabernet Sauvignon',
-            winery='Stag\'s Leap'
-        )
-        client = APIClient()
-        response = client.get('/api/v1/catalog/wines/')
-        self.assertJSONEqual(response.content, [{
-            'country': 'US',
-            'description': 'A delicious bottle of wine.',
-            'id': str(wine.id),
-            'points': 90,
-            'price': '100.00',
-            'variety': 'Cabernet Sauvignon',
-            'winery': 'Stag\'s Leap',
-        }])
+        response = self.client.get("/api/v1/catalog/wines/")
+        wines = Wine.objects.all()
+        self.assertJSONEqual(response.content, WineSerializer(wines, many=True).data)
+
+    def test_query_matches_variety(self):
+        response = self.client.get("/api/v1/catalog/wines/?query=Cabernet")
+        self.assertEquals(1, len(response.data))
+        self.assertEquals("58ba903f-85ff-45c2-9bac-6d0732544841", response.data[0]["id"])
+
+    def test_query_matches_winery(self):
+        response = self.client.get("/api/v1/catalog/wines/?query=Barnard")
+        self.assertEquals(1, len(response.data))
+        self.assertEquals("21e40285-cec8-417c-9a26-4f6748b7fa3a", response.data[0]["id"])
+
+    def test_query_matches_description(self):
+        response = self.client.get('/api/v1/catalog/wines/?query=wine')
+        self.assertEquals(2, len(response.data))
+        self.assertCountEqual([
+            "58ba903f-85ff-45c2-9bac-6d0732544841",
+            "21e40285-cec8-417c-9a26-4f6748b7fa3a",
+        ], [item['id'] for item in response.data])        
