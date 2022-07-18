58fcc0068138e0b934343ebcecd26fc78281b2e1
Kevin Dunn <kgdunn@gmail.com>
2022-07-18 21:08:51 +0000

    Refactor redundancies out.

--- server/catalog/filters.py
+++ server/catalog/filters.py
@@ -1,46 +1,22 @@
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
-from django.db.models import CharField, F, Func, Q, TextField, Value  
+from django.db.models import F, Q  # changed
 
 from django_filters.rest_framework import CharFilter, FilterSet
 
-from .models import Wine
+from .models import Wine, SearchHeadline 
 
 
 class WineFilterSet(FilterSet):
     query = CharFilter(method="filter_query")
 
-   
+    
     def filter_query(self, queryset, name, value):
         search_query = Q(Q(search_vector=SearchQuery(value)))
         return (
             queryset.annotate(
-                variety_headline=Func(
-                    F("variety"),
-                    SearchQuery(value, output_field=CharField()),
-                    Value(
-                        "StartSel = <mark>, StopSel = </mark>, HighlightAll=TRUE",
-                        output_field=CharField(),
-                    ),
-                    function="ts_headline",
-                ),
-                winery_headline=Func(
-                    F("winery"),
-                    SearchQuery(value, output_field=CharField()),
-                    Value(
-                        "StartSel = <mark>, StopSel = </mark>, HighlightAll=TRUE",
-                        output_field=CharField(),
-                    ),
-                    function="ts_headline",
-                ),
-                description_headline=Func(
-                    F("description"),
-                    SearchQuery(value, output_field=TextField()),
-                    Value(
-                        "StartSel = <mark>, StopSel = </mark>, HighlightAll=TRUE",
-                        output_field=TextField(),
-                    ),
-                    function="ts_headline",
-                ),
+                variety_headline=SearchHeadline(F("variety"), SearchQuery(value)),
+                winery_headline=SearchHeadline(F("winery"), SearchQuery(value)),
+                description_headline=SearchHeadline(F("description"), SearchQuery(value)),
                 search_rank=SearchRank(F("search_vector"), SearchQuery(value)),
             )
             .filter(search_query)
