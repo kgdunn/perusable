c4ea943c04b1a485f8d5f9a40ca54ec99e3c35dd
Kevin Dunn <kgdunn@gmail.com>
2022-07-18 20:36:12 +0000

    Add the search vector to postgres.

--- server/catalog/models.py
+++ server/catalog/models.py
@@ -1,5 +1,7 @@
 import uuid
 
+from django.contrib.postgres.indexes import GinIndex
+from django.contrib.postgres.search import SearchVectorField 
 from django.db import models
 
 
@@ -11,6 +13,10 @@ class Wine(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
     variety = models.CharField(max_length=255)
     winery = models.CharField(max_length=255)
+    search_vector = SearchVectorField(null=True, blank=True)  
+
+    class Meta: 
+        indexes = [GinIndex(fields=["search_vector"], name="search_vector_index")]
 
     def __str__(self):
         return f"{self.id}"
