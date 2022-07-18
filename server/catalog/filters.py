bbdae2b13406e024c5e21d9adf787438a509a543
Kevin Dunn <kgdunn@gmail.com>
2022-07-18 21:11:43 +0000

    Move code to the models.py function.

--- server/catalog/filters.py
+++ server/catalog/filters.py
@@ -1,27 +1,13 @@
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
-from django.db.models import F, Q  # changed
-
 from django_filters.rest_framework import CharFilter, FilterSet
 
-from .models import Wine, SearchHeadline 
+from .models import Wine
 
 
 class WineFilterSet(FilterSet):
     query = CharFilter(method="filter_query")
 
-    
     def filter_query(self, queryset, name, value):
-        search_query = Q(Q(search_vector=SearchQuery(value)))
-        return (
-            queryset.annotate(
-                variety_headline=SearchHeadline(F("variety"), SearchQuery(value)),
-                winery_headline=SearchHeadline(F("winery"), SearchQuery(value)),
-                description_headline=SearchHeadline(F("description"), SearchQuery(value)),
-                search_rank=SearchRank(F("search_vector"), SearchQuery(value)),
-            )
-            .filter(search_query)
-            .order_by("-search_rank", "id")
-        )
+        return queryset.search(value)
 
     class Meta:
         model = Wine
--- server/catalog/models.py
+++ server/catalog/models.py
@@ -1,8 +1,24 @@
 import uuid
 
 from django.contrib.postgres.indexes import GinIndex
-from django.contrib.postgres.search import SearchVectorField
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVectorField
 from django.db import models
+from django.db.models import F, Q
+
+
+class WineQuerySet(models.query.QuerySet):
+    def search(self, query):
+        search_query = Q(Q(search_vector=SearchQuery(query)))
+        return (
+            self.annotate(
+                variety_headline=SearchHeadline(F("variety"), SearchQuery(query)),
+                winery_headline=SearchHeadline(F("winery"), SearchQuery(query)),
+                description_headline=SearchHeadline(F("description"), SearchQuery(query)),
+                search_rank=SearchRank(F("search_vector"), SearchQuery(query)),
+            )
+            .filter(search_query)
+            .order_by("-search_rank", "id")
+        )
 
 
 class Wine(models.Model):
@@ -14,6 +30,8 @@ class Wine(models.Model):
     variety = models.CharField(max_length=255)
     winery = models.CharField(max_length=255)
     search_vector = SearchVectorField(null=True, blank=True)
+
+    objects = WineQuerySet.as_manager()
 
     class Meta:
         indexes = [GinIndex(fields=["search_vector"], name="search_vector_index")]
